43aa5f985adb80ec7b75d77a635232e7b86a75bd
Anton Nikolenko <nikolenkoanton92@gmail.com>
2016-01-31 18:20:46 -0800

    add logic to login page

--- public/containers/Login/index.jsx
+++ public/containers/Login/index.jsx
@@ -1,6 +1,53 @@
 var React = require('react')
+var xhr = require('xhr')
 
 module.exports = React.createClass({
+  getInitialState: function() {
+    return {
+      username: '',
+      password: ''
+    }
+  },
+  componentDidMount: function() {
+    this.page = require('page')
+  },
+  handleUsernameChange: function(event) {
+    this.setState({
+      username: event.target.value
+    })
+  },
+  handlePasswordChange: function(event) {
+    this.setState({
+      password: event.target.value
+    })
+  },
+  handleSubmitLogin: function(event) {
+    event.preventDefault()
+    var username = this.state.username.trim()
+    var password = this.state.password.trim()
+
+    if (!username || !password) {
+      return false
+    }
+
+    var self = this
+
+    xhr({
+      method: 'POST',
+      uri: '/login',
+      json: {
+        username: username,
+        password: password
+      }
+    }, function(err, response, body) {
+      if (err || body === 'Unauthorized') {
+        return false
+      } else {
+        self.page.redirect('/')
+      }
+    })
+
+  },
   render: function() {
     return (
       <div className="container">
@@ -11,15 +58,15 @@ module.exports = React.createClass({
               <h3 className="panel-title">Login</h3>
             </div>
             <div className="panel-body">
-              <form>
+              <form onSubmit={this.handleSubmitLogin}>
               <fieldset>
               <div className="form-group">
-                <input className="form-control"  type="text" name="username" placeholder="Your username" />
+                <input className="form-control" onChange={this.handleUsernameChange}  type="text" name="username" placeholder="Your username" />
               </div>
               <div className="form-group">
-                <input className="form-control" type="password" name="password" placeholder="Your password" />
+                <input className="form-control" onChange={this.handlePasswordChange} type="password" name="password" placeholder="Your password" />
               </div>
-              <input className="btn btn-lg btn-success btn-block" type="submit" value="Login" />
+              <button className="btn btn-lg btn-success btn-block" type="submit">Login</button>
               </fieldset>
               </form>
             </div>
