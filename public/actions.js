49c5c90660229920f81cd8f21edab3fbb80927dd
Anton Nikolenko <nikolenkoanton92@gmail.com>
2016-02-19 21:45:49 -0800

    initial actions, add add sample loading for feature page

---
+++ public/actions.js
@@ -0,0 +1,8 @@
+module.exports = {
+  changeTitle: function(tree, title) {
+    tree.set('title', title)
+  },
+  changeFeatureList: function(tree, featureList) {
+    tree.set('featureList', featureList)
+  }
+}
--- public/containers/Feature/index.jsx
+++ public/containers/Feature/index.jsx
@@ -1,5 +1,7 @@
 var React = require('react')
 var mixins = require('baobab-react/mixins')
+var xhr = require('xhr')
+var actions = require('../../actions')
 
 module.exports = React.createClass({
   mixins: [mixins.branch],
@@ -7,11 +9,42 @@ module.exports = React.createClass({
     title: ['title'],
     featureList: ['featureList']
   },
+  actions: {
+    changeTitle: actions.changeTitle,
+    changeFeatureList: actions.changeFeatureList
+  },
+  getInitialState: function() {
+    return {
+      setTitle: 'hello'
+    }
+  },
+  componentDidMount: function() {
+    this.fetchFeatureData()
+  },
+  fetchFeatureData: function() {
+    var self = this
+    xhr({
+      method: 'GET',
+      uri: '/feature',
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    }, function(err, resp, body) {
+      if (err) {
+        return false
+      } else {
+        var data = JSON.parse(body)
+        self.actions.changeTitle(data.title)
+        self.actions.changeFeatureList(data.featureList)
+      }
+
+    })
+  },
   render: function() {
-    var featureList = this.state.featureList.map(function(elementList) {
-      return (
-        <li>{elementList}</li>
-        )
+    var featureList = this.state.featureList && this.state.featureList.map(function(element) {
+        return (
+          <li key={element.id}>{element.name}</li>
+          )
     })
     return (
       <div className="container">
