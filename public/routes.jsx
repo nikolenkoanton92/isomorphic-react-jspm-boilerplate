11f18eb5bdf4dfb42f67d81425644bbfdd54f8d2
Anton Nikolenko <nikolenkoanton92@gmail.com>
2016-02-20 17:54:04 -0800

    add middleware on client for user auth and user not auth

--- public/routes.jsx
+++ public/routes.jsx
@@ -9,13 +9,36 @@ var Router = require('react-router')
 var Route = Router.Route
 var IndexRoute = Router.IndexRoute
 
+function isUserAuth(nextState, replace) {
+  if (!window.tree.get('user', 'id')) {
+    replace({
+      pathname: '/login',
+      state: {
+        nextPathname: nextState.location.pathname
+      }
+    })
+  }
+}
+
+
+function isUserNotAuth(nextState, replace) {
+  if (window.tree.get('user', 'id')) {
+    replace({
+      state: {
+        pathname: '/',
+        nextPathname: nextState.location.pathname
+      }
+    })
+  }
+}
+
 module.exports = (
   <Route name="Home" path="/" component={App} >
    <IndexRoute name="Index" component={Index} />
    <Route name="Feature" path="/feature" component={Feature} />
    <Route name="About" path="/about" component={About} />
-   <Route name="Login" path="/login" component={Login} />
-   <Route name="Dashboard" path="/dashboard" component={Dashboard} />
+   <Route name="Login" path="/login" component={Login} onEnter={isUserNotAuth} />
+   <Route name="Dashboard" path="/dashboard" component={Dashboard} onEnter={isUserAuth}/>
   </Route>
 )
 
